d2ce81a912dc5c46c19d71c3686c6e8b22a8d13a
Joanne Chen <joanne_r_chen@yahoo.com>
2020-10-04 18:00:37 +0100

    can take order and quantity but not outside of menu

--- spec/TakeawaySpec.js
+++ spec/TakeawaySpec.js
@@ -2,20 +2,36 @@
 
 describe('Takeaway', function() {
     let takeaway;
+    let mockMenu;
 
     beforeEach(function() {
         takeaway = new Takeaway;
+        mockMenu = jasmine.createSpyObj('takeaway',['menu'])
     });
     
     it('contains a menu', function() {
         expect(takeaway.menu).toEqual({ "fried rice": 4, "pizza": 8, "chicken curry": 7})
     });
+
+    it('can place an order of a dish and its quantity', function() {
+        takeaway.order("pizza", 1);
+        expect(takeaway.basket).toEqual(["pizza X 1"])
+    })
     
-    // beforeEach(function() {
-    //     mockMenu = jasmine.createSpyObj('mockMenu', ['takeaway.menu'])
-    // })
-    // it('can use a mock menu', function() {
-    //     spyOn(takeaway, '{ "fried rice": 4, "pizza": 8, "chicken curry": 7}').andReturn(mockMenu);
-    //     expect(function() {takeaway.menu; }).toEqual({ "fried rice": 4, "pizza": 8, "chicken curry": 7})
-    // })
+    it('can order many dishes', function() {
+        takeaway.order("pizza", 1);
+        takeaway.order("fried rice", 2)
+        expect(takeaway.basket).toEqual(["pizza X 1", "fried rice X 2"])
+    })
+    
+    it('cannot take an order outside of the menu', function() {
+        takeaway.order("steak", 1);
+        expect(takeaway.basket).toEqual([]);
+    })
+
+   xit('can use a mock menu', function() {
+        // console.log(mockMenu)
+        spyOn(takeaway, 'menu').andReturn({ "fried rice": 4, "pizza": 8, "chicken curry": 7});
+        expect(function() {takeaway.menu; }).toEqual(mockMenu)
+    })
 })
--- src/Takeaway.js
+++ src/Takeaway.js
@@ -3,8 +3,23 @@
 class Takeaway {
     constructor(dishes = new Menu) {
         this.menu = dishes.dishes;
-        console.log(this.menu)
+        this.basket = [];
+
+        // console.log(this.menu)
+    
     };
 
+    // menu() {
+    //     return this.menu
+    //     console.log(this.menu());
+    // }
+    order(dish, quantity) {
+        if (dish != "fried rice" && dish!="pizza" && dish !="chicken curry") {
+            alert("Dish does not exist!");
+        } else {   
+            this.basket.push(`${dish} X ${quantity}`)
+        }
+            return this.basket
+    }
 
 }
