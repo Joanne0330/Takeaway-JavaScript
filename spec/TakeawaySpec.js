75d17f0a53e34af523cf4e21399c85888e886938
Joanne Chen <joanne_r_chen@yahoo.com>
2020-10-06 13:28:03 +0100

    order summary now gives a string of order summary and total

--- spec/TakeawaySpec.js
+++ spec/TakeawaySpec.js
@@ -13,6 +13,12 @@ describe('Takeaway', function() {
         expect(takeaway.menu).toEqual({ "fried rice": 4, "pizza": 8, "chicken curry": 7})
     });
 
+    xit('can use a mock menu', function() {
+         // console.log(mockMenu)
+         spyOn(takeaway, 'menu').andReturn({ "fried rice": 4, "pizza": 8, "chicken curry": 7});
+         expect(function() {takeaway.menu; }).toEqual(mockMenu)
+     })
+
     it('can place an order of a dish and its quantity', function() {
         takeaway.order("pizza", 1);
         expect(takeaway.basket).toEqual({"pizza": 1})
@@ -29,9 +35,23 @@ describe('Takeaway', function() {
         expect(takeaway.basket).toEqual({});
     })
 
-   xit('can use a mock menu', function() {
-        // console.log(mockMenu)
-        spyOn(takeaway, 'menu').andReturn({ "fried rice": 4, "pizza": 8, "chicken curry": 7});
-        expect(function() {takeaway.menu; }).toEqual(mockMenu)
+    it('calculates the sum of the order', function() {
+        takeaway.order("pizza", 2);
+        takeaway.orderSummary();
+        expect(takeaway.sum).toEqual(16)
+    })
+
+    it('calculates the sum of multiple orders', function() {
+        takeaway.order("pizza", 2);
+        takeaway.order("chicken curry", 3)
+        takeaway.orderSummary();
+        expect(takeaway.sum).toEqual(37)
+    })
+
+    it('has a order summary at the checkout', function(){
+        takeaway.order("pizza", 2);
+        takeaway.order("chicken curry", 3)
+        takeaway.orderSummary();
+        expect(takeaway.summaryText).toEqual("Thank you! Your order: pizza X 2, chicken curry X 3 and the total is £37")
     })
 })
--- src/Takeaway.js
+++ src/Takeaway.js
@@ -6,22 +6,28 @@ class Takeaway {
         this.basket = {}; 
         this.summary = []
         this.sum = 0;
+        this.summaryText = "";
     };
 
     order(dish, quantity) {
         for (var key in this.menu) {
             // if (this.menu.hasOwnProperty(dish) === true) {
             if (dish == "fried rice" || dish =="pizza" || dish == "chicken curry") {
-                this.summary.push(`${dish} X ${quantity}`);  
                 this.basket[dish] = quantity; 
-            };
+            }
         }
+        this.summary.push(` ${dish} X ${quantity}`);  
         return this.basket  
     }
 
-    // total(arguments) {
-    //     console.log(arguments[0])
-    // } 
+    orderSummary() {
+        for (var key in this.basket) {
+            this.sum += (this.menu[key] * this.basket[key]);
+        }
+        // console.log(`Thank you! Your order:${this.summary} and the total is £${this.sum}`)
+        this.summaryText = `Thank you! Your order:${this.summary} and the total is £${this.sum}`
+        return this.summaryText
+    } 
 
 
 }
